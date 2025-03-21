981da97badf3fc6c5649bd4c04f79a7f21333f58
Devendra-Prajapat-Inventyv <devendra.prajapati.inventyv@gmail.com>
2025-03-21 16:44:53 +0530

    Project added

---
+++ my_app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+    
+        <a>
+          <h1>Hello Devendra</h1>
+        </a>
+    </div>
+  );
+}
+
+export default App;
